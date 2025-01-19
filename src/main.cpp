0abe181425cc63ec562166313528a1e9b75f58cc
drfailer <drfailer.42@gmail.com>
2025-01-19 16:07:15 -0500

    makeLambdaTask function

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include "hh/lambda_task.h"
 #include <hedgehog/hedgehog.h>
 
-/******************************************************************************/
-/*                                    main                                    */
-/******************************************************************************/
+// this functions should be in hh v
+template <size_t Separator, typename... AllTypes>
+auto makeLambdaTask(std::string const &name, auto lambda,
+                    size_t numberThreads = 1) {
+  return std::make_shared<
+      hh::LambdaTask<decltype(lambda), Separator, AllTypes...>>(name, lambda,
+                                                                numberThreads);
+}
 
 int main() {
-  auto f =
-      []<typename T>(std::shared_ptr<T> data, auto *pThis) {
+  auto Simple = makeLambdaTask<2, int, double, int>(
+      "Simple", []<typename T>(std::shared_ptr<T> data, auto *pThis) {
         if constexpr (std::is_same_v<T, int>) {
           (*data)++;
           printf("[Simple][Data %d][Device ID %d]\n", *data, pThis->deviceId());
@@ -19,10 +24,7 @@ int main() {
           /* pThis->getManagedMemory(); */
           pThis->addResult(std::make_shared<int>(*data));
         }
-      };
-
-  auto Simple = std::make_shared<hh::LambdaTask<decltype(f), 2, int, double, int>>(
-      "Simple", f);
+      });
 
   hh::Graph<2, int, double, int> graph("test");
 
