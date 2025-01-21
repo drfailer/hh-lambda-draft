e34ac1addc9b0ab2c0fe75d01ce9536030f7549e
drfailer <drfailer.42@gmail.com>
2025-01-21 09:27:31 -0500

    test: it works
    todo: add variadic constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,24 @@
 #include "hh/lambda_task.h"
 #include <hedgehog/hedgehog.h>
 
-// this functions should be in hh v
-template <size_t Separator, typename... AllTypes>
-auto makeLambdaTask(std::string const &name, auto lambda,
-                    size_t numberThreads = 1) {
-  return std::make_shared<
-      hh::LambdaTask<decltype(lambda), Separator, AllTypes...>>(name, lambda,
-                                                                numberThreads);
-}
-
 int main() {
-  auto Simple = makeLambdaTask<2, int, double, int>(
-      "Simple", []<typename T>(std::shared_ptr<T> data, auto *pThis) {
-        if constexpr (std::is_same_v<T, int>) {
-          (*data)++;
-          printf("[Simple][Data %d][Device ID %d]\n", *data, pThis->deviceId());
-          /* pThis->getManagedMemory(); */
-          pThis->addResult(data);
-        } else if constexpr (std::is_same_v<T, double>) {
-          (*data)++;
-          printf("[Simple][Data %f][Device ID %d]\n", *data, pThis->deviceId());
-          /* pThis->getManagedMemory(); */
-          pThis->addResult(std::make_shared<int>(*data));
-        }
-      });
+  auto Simple = std::make_shared<hh::LambdaTask<2, int, double, int>>(
+      "Simple", std::tuple{
+                    [](std::shared_ptr<int> data, auto *pThis) {
+                      (*data)++;
+                      printf("[Simple<int>][Data %d][Device ID %d]\n", *data,
+                             pThis->deviceId());
+                      /* pThis->getManagedMemory(); */
+                      pThis->addResult(data);
+                    },
+                    [](std::shared_ptr<double> data, auto *pThis) {
+                      (*data)++;
+                      printf("[Simple<double>][Data %f][Device ID %d]\n", *data,
+                             pThis->deviceId());
+                      /* pThis->getManagedMemory(); */
+                      pThis->addResult(std::make_shared<int>(*data));
+                    },
+                });
 
   hh::Graph<2, int, double, int> graph("test");
 
